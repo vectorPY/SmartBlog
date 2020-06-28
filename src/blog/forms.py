215d5278c64d65465b69c21f839ea3c634b512ed
vectorPY <vector.thedev@gmail.com>
2020-06-28 14:15:18 +0200

    Changes

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 from .models import Blog
+from django.forms import Select
+
+sections = [
+    ('Food', 'Food'),
+    ('Gaming', 'Gaming'),
+    ('Programming', 'Programming'),
+    ('Technology', 'Technology'),
+    ('Stories', 'Stories'),
+]
 
 
 class RawBlogForm(forms.ModelForm):
@@ -7,28 +16,8 @@ class RawBlogForm(forms.ModelForm):
         model = Blog
         fields = ['author', 'title', 'section', 'short_version', 'text', 'image']
 
-        def clean_author(self):
-            author = self.cleaned_data.get('author')
-            if len(author) > 150:
-                raise forms.ValidationError("Your name is too long")
-            elif len(author) < 5:
-                raise forms.ValidationError("Your name is too short")
-            else:
-                return author
+        widgets = {
+            'section': Select(choices=sections)
+        }
 
-        def clean_title(self):
-            title = self.cleaned_data.get('title')
-            if len(title) > 150:
-                raise forms.ValidationError("Your title is too long")
-            elif len(title) < 5:
-                raise forms.ValidationError("Your title is too short")
-            else:
-                return title
-
-        def clean_shortVersion(self):
-            short = self.cleaned_data.get('shortVersion')
-            if len(short) > 600:
-                raise forms.ValidationError("The description is too long")
-            else:
-                return short
 
