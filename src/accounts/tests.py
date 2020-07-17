85693b44a2fa14a03b8b87b0b6431f2bd408a886
vectorPY <vector.thedev@gmail.com>
2020-07-17 23:27:17 +0200

    Updated tests.py

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from accounts.models import Banned, ExemptionRequest
+
 
 # Create your tests here.
+class BannedTest(TestCase):
+    def setUp(self) -> None:
+        self.banned = Banned(banned_by="vector", user="test", reason="nothing")
+
+    def test_ban(self):
+        self.assertEqual(self.banned.banned_by, "vector")
+        self.assertEqual(self.banned.user, "test")
+        self.assertEqual(self.banned.reason, "nothing")
+
+
+class ExemptionRequestTest(TestCase):
+    def setUp(self) -> None:
+        self.exemp_req = ExemptionRequest(user="user", banned_by="vector", reason="nothing")
+
+    def test_exemp_req(self):
+        self.assertEqual(self.exemp_req.user, "user")
+        self.assertEqual(self.exemp_req.banned_by, "vector")
+        self.assertEqual(self.exemp_req.reason, "nothing")
+
